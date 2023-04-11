37b43386ee88bb1917c4bf95f005d136b48c12a5
EriYukai <lvupmaker@gmail.com>
2023-04-11 13:22:24 +0900

    aa1111

--- api.js
+++ api.js
@@ -101,25 +101,7 @@ function getNearbyRestaurants(latitude, longitude) {
     });
 }
 
-async function getRestaurantImage(placeUrl) {
-  const KAKAO_PLACE_API_URL = "https://dapi.kakao.com/v2/local/search/keyword.json";
-  const query = "음식점";
 
-  const headers = {
-    Authorization: `KakaoAK ${KAKAO_API_KEY}`,
-    "Content-Type": "application/json;charset=UTF-8",
-    "Accept": "application/json",
-    "KA": "sdk/1.38.0 os/javascript lang/en-US device/Win32 origin/https%3A%2F%2Feriyukai.github.io"
-  };
-
-  const url = `${KAKAO_PLACE_API_URL}?query=${query}&page=1&size=1&sort=accuracy&query=${encodeURIComponent(placeUrl)}`;
-  
-  const response = await fetch(url, { headers });
-  const data = await response.json();
-
-  const imageUrl = data.documents[0].place_photo[0].thumbnail_url;
-  return imageUrl;
-}
 
 async function getNearbyRestaurants(latitude, longitude) {
   const KAKAO_SEARCH_API_URL = "https://dapi.kakao.com/v2/local/search/category.json";
@@ -144,6 +126,26 @@ async function getNearbyRestaurants(latitude, longitude) {
     });
 }
 
+
+async function getRestaurantImage(placeUrl) {
+  const KAKAO_PLACE_API_URL = "https://dapi.kakao.com/v2/local/search/keyword.json";
+  const query = "음식점";
+
+  const headers = {
+    Authorization: `KakaoAK ${KAKAO_API_KEY}`,
+    "Content-Type": "application/json;charset=UTF-8",
+    "Accept": "application/json",
+    "KA": "sdk/1.38.0 os/javascript lang/en-US device/Win32 origin/https%3A%2F%2Feriyukai.github.io"
+  };
+
+  const url = `${KAKAO_PLACE_API_URL}?query=${query}&page=1&size=1&sort=accuracy&query=${encodeURIComponent(placeUrl)}`;
+  
+  const response = await fetch(url, { headers });
+  const data = await response.json();
+
+  const imageUrl = data.documents[0].place_photo[0].thumbnail_url;
+  return imageUrl;
+}
 
 const restaurantImageUrl = restaurant.place_url ? await getRestaurantImage(restaurant.place_url) : "./images/ys.jpg";
 
