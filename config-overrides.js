b52ac32c8c3dfb7b74071f9e82fff141e46fb190
EriYukai <lvupmaker@gmail.com>
2023-04-14 12:29:29 +0900

    1212

--- config-overrides.js
+++ config-overrides.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = function override(config, env) {
   config.resolve.alias['public'] = path.resolve(__dirname, 'public');
+    config.entry = path.resolve(__dirname, 'src', 'index.js');
   config.plugins.forEach((plugin) => {
     if (plugin.constructor.name === 'HtmlWebpackPlugin') {
       plugin.options.template = path.resolve(__dirname, 'index.html');
