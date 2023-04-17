aa88b4cdf3c457bd85e7e11898c29d4cef88147f
EriYukai <lvupmaker@gmail.com>
2023-04-17 10:04:14 +0900

    tt21

--- functions/generate-text.js
+++ functions/generate-text.js
@@ -1,47 +1,35 @@
+// root/functions/generate-text.js
 const fetch = require('node-fetch');
 
 exports.handler = async function (event, context) {
-  // GPT API 호출에 필요한 정보
-  const KAKAO_API_KEY = "14f09bd760730c467aa000cb14fbb7e0";
-  const KAKAO_APP_KEY = "a5f5f6ab161a7b4e31d6bd02bd4547e6";
-  const apiKey = process.env.GPT_API_KEY;
-  const gptApiUrl = "https://api.openai.com/v1/engines/gpt-3.5-turbo/completions";
+  const KAKAO_API_KEY = process.env.KAKAO_API_KEY;
+  const KAKAO_SEARCH_API_URL = "https://dapi.kakao.com/v2/local/search/category.json";
+  const category_group_code = "FD6"; // 음식점 카테고리 코드
+  const radius = 5000; // 반경 2km 내 검색
 
-
-  // 클라이언트로부터 받은 데이터
   const data = JSON.parse(event.body);
+  const { latitude, longitude } = data;
+  const url = `${KAKAO_SEARCH_API_URL}?category_group_code=${category_group_code}&x=${longitude}&y=${latitude}&radius=${radius}`;
 
+  const headers = {
+    Authorization: `KakaoAK ${KAKAO_API_KEY}`,
+    "Content-Type": "application/json;charset=UTF-8",
+    "Accept": "application/json",
+    "KA": "sdk/1.38.0 os/javascript lang/en-US device/Win32 origin/https%3A%2F%2Feriyukai.github.io",
+  };
 
-  data.prompt = "너는 최고의 음식평론가야 너는 다정한 말투로 음식점을 추천해주는 존재야"
+  try {
+    const response = await fetch(url, { headers });
+    const data = await response.json();
 
-// GPT API 호출 코드 작성
-const response = await fetch(gptApiUrl, {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Authorization": `Bearer ${apiKey}`,
-  },
-  body: JSON.stringify({
-    model: "gpt-3.5-turbo",
-    messages: [
-      { role: "user", content: data.prompt },
-    ],
-    max_tokens: 500,
-    temperature: 0.5,
-    top_p: 1,
-    frequency_penalty: 0.9,
-    presence_penalty: 0.9,
-    timeout: 900,
-  }),
-});
-
-const gptResponse = await response.json();
-const result = gptResponse.choices[0].message.content.strip();
-
-
-// 결과 반환
-return {
-  statusCode: 200,
-  body: JSON.stringify({ result }),
-};
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
 };
