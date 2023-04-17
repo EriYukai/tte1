650621ecdbc62e29ad097bbb4ad82bdb24607929
EriYukai <lvupmaker@gmail.com>
2023-04-17 15:31:42 +0900

    ㅅㅂ1

--- public/api.js
+++ public/api.js
@@ -171,7 +171,7 @@ async function getGptResponse(restaurants) {
   const requestBody = {
     prompt: `${prompt}\n${restaurantList}\n`,
     temperature: 0.7,
-    max_tokens: 60,
+    max_tokens: 2000,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
