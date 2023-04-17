9b0e0d9d7409a91273043ad48d617f0c4383879a
EriYukai <lvupmaker@gmail.com>
2023-04-17 11:04:10 +0900

    zzzx1

--- public/api.js
+++ public/api.js
@@ -57,7 +57,7 @@ function showPosition(position) {
 
 async function getNearbyRestaurants(latitude, longitude) {
   try {
-    const response = await fetch("/.netlify/functions/get-nearby-restaurants", {
+    const response = await fetch("/.netlify/functions/generate-text", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
