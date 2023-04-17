348229eba06278273847ebb262e3a3b522b62ebe
EriYukai <lvupmaker@gmail.com>
2023-04-17 17:51:45 +0900

    55555123

--- public/api.js
+++ public/api.js
@@ -187,20 +187,39 @@ function showError(error) {
 
 async function displayRestaurantInfo(restaurant) {
   const restaurantName = restaurant.place_name;
-  const KAKAO_DETAIL_API_URL = "https://dapi.kakao.com/v2/local/search/keyword.json";
+  const KAKAO_SEARCH_API_URL = "https://dapi.kakao.com/v2/local/search/keyword.json";
+  const KAKAO_DETAIL_API_URL = "https://dapi.kakao.com/v2/local/search/place.json";
   const headers = {
-    Authorization: `KakaoAK ${process.env.KAKAO_API_KEY}`
+    Authorization: "KakaoAK <YOUR_APP_REST_API_KEY>"
   };
-  const keyword = encodeURIComponent(restaurantName);
-  const url = `${KAKAO_DETAIL_API_URL}?query=${keyword}`;
-
+  
   try {
-    const response = await fetch(url, { headers });
-    const data = await response.json();
+    // 음식점 검색
+    const query = encodeURIComponent(restaurantName);
+    const category_group_code = "FD6"; // 음식점 카테고리 코드
+    const radius = 5000; // 반경 5km 내 검색
+    const searchUrl = `${KAKAO_SEARCH_API_URL}?query=${query}&category_group_code=${category_group_code}&radius=${radius}`;
+    const searchResponse = await fetch(searchUrl, { headers });
+    const searchData = await searchResponse.json();
+    
+    // 검색 결과가 없는 경우
+    if (!searchData.documents || searchData.documents.length === 0) {
+      console.log("검색 결과가 없습니다.");
+      return;
+    }
+    
+    // 검색 결과 중 첫번째 음식점 선택
+    const placeId = searchData.documents[0].id;
 
-    // 이미지 URL 가져오기
-    const imageUrl = data.documents[0].place_url;
+    // 음식점 상세 정보 검색
+    const detailUrl = `${KAKAO_DETAIL_API_URL}?place_id=${placeId}`;
+    const detailResponse = await fetch(detailUrl, { headers });
+    const detailData = await detailResponse.json();
 
+    // 음식점 이미지 URL 가져오기
+    const imageUrl = detailData.documents[0].place_url;
+
+    // 이미지 표시
     const imageElement = document.querySelector("#restaurant-image-tag");
     imageElement.src = imageUrl;
 
