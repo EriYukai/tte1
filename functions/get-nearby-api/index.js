5a34a77891e51748ee84ad923dae619775248f18
EriYukai <lvupmaker@gmail.com>
2023-04-18 17:00:53 +0900

    123123

--- functions/get-nearby-api/index.js
+++ functions/get-nearby-api/index.js
@@ -1,63 +1,36 @@
+// root/functions/generate-text.js
 const fetch = (...args) => import('node-fetch').then(({default: f}) => f(...args));
 
-const fetchCategoryData = async (category_group_code, longitude, latitude, radius) => {
+exports.handler = async function (event, context) {
   const KAKAO_API_KEY = process.env.KAKAO_API_KEY;
-  const KAKAO_SEARCH_API_URL = "https://dapi.kakao.com/v2/local/search/keyword.json";
-  const KAKAO_DETAIL_API_URL = "https://dapi.kakao.com/v2/local/search/place.json";
-  const KAKAO_CATEGORY_API_URL = "https://dapi.kakao.com/v2/local/search/category.json";
+  const KAKAO_SEARCH_API_URL = "https://dapi.kakao.com/v2/local/search/category.json";
+  const category_group_code = "FD6"; // 음식점 카테고리 코드
+  const radius = 5000; // 반경 2km 내 검색
+
+  const data = JSON.parse(event.body);
+  const { latitude, longitude } = data;
+  const url = `${KAKAO_SEARCH_API_URL}?category_group_code=${category_group_code}&x=${longitude}&y=${latitude}&radius=${radius}`;
 
   const headers = {
-    "Authorization": `KakaoAK ${KAKAO_API_KEY}`,
-    "Content-Type": "application/json"
+    Authorization: `KakaoAK ${KAKAO_API_KEY}`,
+    "Content-Type": "application/json;charset=UTF-8",
+    "Accept": "application/json",
+    "KA": "sdk/1.38.0 os/javascript lang/en-US device/Win32 origin/https%3A%2F%2Feriyukai.github.io",
   };
 
-  const categoryUrl = `${KAKAO_CATEGORY_API_URL}?category_group_code=${category_group_code}&x=${longitude}&y=${latitude}&radius=${radius}`;
-  const categoryResponse = await fetch(categoryUrl, { headers });
-  const categoryData = await categoryResponse.json();
-
-  return categoryData;
-};
-
-const createResponse = (statusCode, body) => ({
-  statusCode,
-  body: JSON.stringify(body),
-});
-
-exports.handler = async function (event, context) {
-  console.log("event.body:", event.body);
-  
-  if (!event.body) {
-    return createResponse(400, { error: "Missing event body" });
-  }
-  
-  let latitude, longitude;
-  
   try {
-    const parsedBody = JSON.parse(event.body);
-    latitude = parsedBody.latitude;
-    longitude = parsedBody.longitude;
+    const response = await fetch(url, { headers });
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
   } catch (error) {
-    console.error("Error parsing event body:", error);
-    return createResponse(400, { error: "Invalid JSON in event body" });
-  }
-  
-  const category_group_code = "FD6"; // 음식점 카테고리 코드
-  const radius = 2000; // 반경 2km 내 검색
-
-  try {
-    const categoryData = await fetchCategoryData(category_group_code, longitude, latitude, radius);
-
-    if (!categoryData.documents || categoryData.documents.length === 0) {
-      return createResponse(204, null); // 수정된 부분
-    }
-    
-
-    const response = createResponse(200, categoryData);
-    console.log("response:", response);
-
-    return response;
-  } catch (error) {
-    console.error(error);
-    return createResponse(500, { error: error.message });
+    console.error(error); // 에러를 콘솔에 출력하도록 추가
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
   }
 };
