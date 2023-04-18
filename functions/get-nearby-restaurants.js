7229ccc94439e7a2194b8e6c7dad24370640aa22
EriYukai <lvupmaker@gmail.com>
2023-04-18 10:02:24 +0900

    551

--- functions/get-nearby-restaurants.js
+++ functions/get-nearby-restaurants.js
@@ -1,33 +1,51 @@
-// root/functions/get-nearby-restaurants.js
 const fetch = (...args) => import('node-fetch').then(({default: f}) => f(...args));
 
 exports.handler = async function (event, context) {
   const KAKAO_API_KEY = process.env.KAKAO_API_KEY;
-  const KAKAO_SEARCH_API_URL = "https://dapi.kakao.com/v2/local/search/category.json";
-  const category_group_code = "FD6"; // 음식점 카테고리 코드
-  const radius = 5000; // 반경 2km 내 검색
+  const KAKAO_SEARCH_API_URL = "https://dapi.kakao.com/v2/local/search/keyword.json";
+  const KAKAO_DETAIL_API_URL = "https://dapi.kakao.com/v2/local/search/place.json";
 
   const data = JSON.parse(event.body);
-  const { latitude, longitude } = data;
-  const url = `${KAKAO_SEARCH_API_URL}?category_group_code=${category_group_code}&x=${longitude}&y=${latitude}&radius=${radius}`;
+  const { restaurantName } = data;
 
   const headers = {
-    Authorization: `KakaoAK ${KAKAO_API_KEY}`,
-    "Content-Type": "application/json;charset=UTF-8",
-    "Accept": "application/json",
-    "KA": "sdk/1.38.0 os/javascript lang/en-US device/Win32 origin/https%3A%2F%2Feriyukai.github.io",
+    "Authorization": `KakaoAK ${KAKAO_API_KEY}`,
+    "Content-Type": "application/json"
   };
 
   try {
-    const response = await fetch(url, { headers });
-    const data = await response.json();
+    const query = encodeURIComponent(restaurantName);
+    const category_group_code = "FD6"; // 음식점 카테고리 코드
+    const radius = 5000; // 반경 5km 내 검색
+    const searchUrl = `${KAKAO_SEARCH_API_URL}?query=${query}&category_group_code=${category_group_code}&radius=${radius}`;
+    const searchResponse = await fetch(searchUrl, { headers });
+    const searchData = await searchResponse.json();
+
+    if (!searchData.documents || searchData.documents.length === 0) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: "검색 결과가 없습니다." }),
+      };
+    }
+
+    const placeId = searchData.documents[0].id;
+    const detailUrl = `${KAKAO_DETAIL_API_URL}?place_id=${placeId}`;
+    const detailResponse = await fetch(detailUrl, { headers });
+    const detailData = await detailResponse.json();
+
+    if (!detailData.documents || detailData.documents.length === 0) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: "상세 정보가 없습니다." }),
+      };
+    }
 
     return {
       statusCode: 200,
-      body: JSON.stringify(data),
+      body: JSON.stringify(detailData),
     };
-} catch (error) {
-    console.error(error); // 에러를 콘솔에 출력하도록 추가
+  } catch (error) {
+    console.error(error);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: error.message }),
