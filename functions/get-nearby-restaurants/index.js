9dfb311a6375287c72293bdfdd2c0685e02595f3
EriYukai <lvupmaker@gmail.com>
2023-04-19 10:30:20 +0900

    1231231

--- functions/get-nearby-restaurants/index.js
+++ functions/get-nearby-restaurants/index.js
@@ -45,3 +45,40 @@ exports.getRestaurantDetails = async function (event, context) {
     return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
   }
 };
+
+// root/functions/generate-text.js
+const fetch = (...args) => import('node-fetch').then(({default: f}) => f(...args));
+
+exports.handler = async function (event, context) {
+  const KAKAO_API_KEY = process.env.KAKAO_API_KEY;
+  const KAKAO_SEARCH_API_URL = "https://dapi.kakao.com/v2/local/search/category.json";
+  const category_group_code = "FD6"; // 음식점 카테고리 코드
+  const radius = 5000; // 반경 2km 내 검색
+
+  const data = JSON.parse(event.body);
+  const { latitude, longitude } = data;
+  const url = `${KAKAO_SEARCH_API_URL}?category_group_code=${category_group_code}&x=${longitude}&y=${latitude}&radius=${radius}`;
+
+  const headers = {
+    Authorization: `KakaoAK ${KAKAO_API_KEY}`,
+    "Content-Type": "application/json;charset=UTF-8",
+    "Accept": "application/json",
+    "KA": "sdk/1.38.0 os/javascript lang/en-US device/Win32 origin/https%3A%2F%2Feriyukai.github.io",
+  };
+
+  try {
+    const response = await fetch(url, { headers });
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    console.error(error); // 에러를 콘솔에 출력하도록 추가
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
