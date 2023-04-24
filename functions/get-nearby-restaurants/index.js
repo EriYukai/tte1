11e5d7db8feaa761bd88857aa57f644b2a23c33b
EriYukai <lvupmaker@gmail.com>
2023-04-24 15:09:12 +0900

    qqw12

--- functions/get-nearby-restaurants/index.js
+++ functions/get-nearby-restaurants/index.js
@@ -1,9 +1,21 @@
 const fetch = (...args) => import('node-fetch').then(({default: f}) => f(...args));
 
+async function searchRestaurant(query, headers, category_group_code, radius) {
+  const KAKAO_SEARCH_API_URL = "https://dapi.kakao.com/v2/local/search/keyword.json";
+  const searchUrl = `${KAKAO_SEARCH_API_URL}?query=${query}&category_group_code=${category_group_code}&radius=${radius}`;
+  const searchResponse = await fetch(searchUrl, { headers });
+  return await searchResponse.json();
+}
+
+async function getRestaurantDetails(placeId, headers) {
+  const KAKAO_DETAIL_API_URL = "https://dapi.kakao.com/v2/local/search/place.json";
+  const detailUrl = `${KAKAO_DETAIL_API_URL}?place_id=${placeId}`;
+  const detailResponse = await fetch(detailUrl, { headers });
+  return await detailResponse.json();
+}
+
 exports.handler = async function (event, context) {
   const KAKAO_API_KEY = process.env.KAKAO_API_KEY;
-  const KAKAO_SEARCH_API_URL = "https://dapi.kakao.com/v2/local/search/keyword.json";
-  const KAKAO_DETAIL_API_URL = "https://dapi.kakao.com/v2/local/search/place.json";
 
   if (!event.body) {
     console.error("Empty request body");
@@ -34,9 +46,7 @@ exports.handler = async function (event, context) {
     const query = encodeURIComponent(restaurantName);
     const category_group_code = "FD6"; // 음식점 카테고리 코드
     const radius = 5000; // 반경 5km 내 검색
-    const searchUrl = `${KAKAO_SEARCH_API_URL}?query=${query}&category_group_code=${category_group_code}&radius=${radius}`;
-    const searchResponse = await fetch(searchUrl, { headers });
-    const searchData = await searchResponse.json();
+    const searchData = await searchRestaurant(query, headers, category_group_code, radius);
 
     if (!searchData.documents || searchData.documents.length === 0) {
       return {
@@ -46,9 +56,7 @@ exports.handler = async function (event, context) {
     }
 
     const placeId = searchData.documents[0].id;
-    const detailUrl = `${KAKAO_DETAIL_API_URL}?place_id=${placeId}`;
-    const detailResponse = await fetch(detailUrl, { headers });
-    const detailData = await detailResponse.json();
+    const detailData = await getRestaurantDetails(placeId, headers);
 
     if (!detailData.documents || detailData.documents.length === 0) {
       return {
