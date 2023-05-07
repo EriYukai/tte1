1bff81da4f7787ebf53a4110d215c29e7d5d3532
EriYukai <lvupmaker@gmail.com>
2023-05-07 14:45:49 +0900

    12312qweqwe

--- public/scripts.js
+++ public/scripts.js
@@ -498,22 +498,21 @@ function addFadingDot(x, y) {
 async function displayRestaurants(restaurants, latitude, longitude) {
   const restaurantList = document.getElementById("restaurant-list");
   restaurantList.innerHTML = "";
-  
-  
-  // 첫 번째 음식점 정보를 선택합니다.
-  const selectedRestaurant = restaurants[0];
-
 
   for (let i = 0; i < restaurants.length; i++) {
     const restaurant = restaurants[i];
-    const listItem = document.createElement("li");
-    listItem.innerHTML = `
-      <h3>${restaurant.title}</h3>
-      <p>${restaurant.address}</p>
-      <p>${restaurant.category}</p>
-      <button data-index="${i}" data-place-id="${restaurant.id}">자세히 보기</button>
-    `;
-    restaurantList.appendChild(listItem);
+    
+    // 음식점 객체 유효성 검사
+    if (restaurant && restaurant.image_url && restaurant.id) {
+      const listItem = document.createElement("li");
+      listItem.innerHTML = `
+        <h3>${restaurant.title}</h3>
+        <p>${restaurant.address}</p>
+        <p>${restaurant.category}</p>
+        <button data-index="${i}" data-place-id="${restaurant.id}">자세히 보기</button>
+      `;
+      restaurantList.appendChild(listItem);
+    }
   }
 
   // 이 부분을 추가하세요.
@@ -524,32 +523,8 @@ async function displayRestaurants(restaurants, latitude, longitude) {
       await displayRestaurantInfo(placeId);
     });
   }
-
-  // 이미지를 삽입할 div를 선택합니다.
-  const contentArea = document.querySelector(".content-area");
-  contentArea.innerHTML = ""; // 이전 이미지를 제거합니다.
-
-  // 음식점 이미지 출력
-  const imageElement = document.createElement("img");
-  imageElement.id = "restaurant-image-tag";
-  imageElement.src = selectedRestaurant.image_url;
-  imageElement.alt = selectedRestaurant.title;
-
-  // 이미지를 content-area div에 추가합니다.
-  contentArea.appendChild(imageElement);
-
-  // 음식점 이름 출력
-  const nameElement = document.querySelector("#restaurant-name");
-  nameElement.textContent = selectedRestaurant.title;
-
-  // 음식점 주소 출력
-  const addressElement = document.querySelector("#restaurant-address");
-  addressElement.textContent = selectedRestaurant.address;
-
-  // 음식점 전화번호 출력
-  const phoneElement = document.querySelector("#restaurant-phone");
-  phoneElement.textContent = selectedRestaurant.telephone;
 }
+
 
 
 
