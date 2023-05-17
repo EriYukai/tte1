7f753488f87be913670dd679fb2acbd9ddc59c7a
EriYukai <lvupmaker@gmail.com>
2023-05-17 09:43:51 +0900

    wqrqwe1

--- functions/get-nearby-restaurants/index.js
+++ functions/get-nearby-restaurants/index.js
@@ -1,43 +1,55 @@
 // get-nearby-restaurants/index.js
 
 exports.handler = async function(event, context) {
-  const { lat, lng, apiKey } = JSON.parse(event.body);
+  // Log the event body to debug
+  console.log('Event body:', event.body);
 
-  // Dynamically import node-fetch
-  const fetch = (await import('node-fetch')).default;
+  // Proceed if event.body is not empty
+  if (event.body) {
+    const { lat, lng, apiKey } = JSON.parse(event.body);
 
-  // Fetch nearby restaurants
-  const restaurantsResponse = await fetch(`https://dapi.kakao.com/v2/local/search/keyword.json?query=음식점&radius=2000&x=${lng}&y=${lat}`, {
-    headers: {
-      Authorization: `KakaoAK ${apiKey}`
-    }
-  });
-  const restaurantsData = await restaurantsResponse.json();
-  const restaurants = restaurantsData.documents.slice(0, 15);
+    // Dynamically import node-fetch
+    const fetch = (await import('node-fetch')).default;
 
-  // Fetch restaurant recommendations
-  const YOUR_OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-  const recommendationResponse = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${YOUR_OPENAI_API_KEY}`
-    },
-    body: JSON.stringify({
-      prompt: `Here are some restaurants: ${restaurants.map(r => r.place_name).join(', ')}. Which one should I choose?`,
-      max_tokens: 60
-    })
-  });
-  const recommendationData = await recommendationResponse.json();
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      restaurants,
-      recommendation: {
-        name: 'Recommended restaurant',
-        reason: recommendationData.choices[0].text.trim()
+    // Fetch nearby restaurants
+    const restaurantsResponse = await fetch(`https://dapi.kakao.com/v2/local/search/keyword.json?query=음식점&radius=2000&x=${lng}&y=${lat}`, {
+      headers: {
+        Authorization: `KakaoAK ${apiKey}`
       }
-    })
-  };
+    });
+    const restaurantsData = await restaurantsResponse.json();
+    const restaurants = restaurantsData.documents.slice(0, 15);
+
+    // Fetch restaurant recommendations
+    const YOUR_OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+    const recommendationResponse = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${YOUR_OPENAI_API_KEY}`
+      },
+      body: JSON.stringify({
+        prompt: `Here are some restaurants: ${restaurants.map(r => r.place_name).join(', ')}. Which one should I choose?`,
+        max_tokens: 60
+      })
+    });
+    const recommendationData = await recommendationResponse.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        restaurants,
+        recommendation: {
+          name: 'Recommended restaurant',
+          reason: recommendationData.choices[0].text.trim()
+        }
+      })
+    };
+  } else {
+    // Return an error if event.body is empty
+    return {
+      statusCode: 400,
+      body: 'Invalid request body'
+    };
+  }
 }
